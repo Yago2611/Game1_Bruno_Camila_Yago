ed8d00d0ca34f780311baa3cbaf593e515e180a0
Yago2611 <116475207+Yago2611@users.noreply.github.com>
2022-11-01 09:06:01 -0300

    Update cena_inicial.py

--- cena_inicial.py
+++ cena_inicial.py
@@ -1,2 +1,9 @@
+import pygame as pg
+import sys 
+
 class Cena_Inicial:
-  pass
+  def __init__(self,tela):
+    self.tela = tela
+    font_titulo = pg.font.SysFont(None, 12)
+    self.titulo = font_titulo.render(f'Escolha um personagem', True, (0,0,0))
+  def 
