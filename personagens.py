e644ffdc5329b44a8706282576ff2ab4a9bd696c
Bruno8652 <116586486+Bruno8652@users.noreply.github.com>
2022-11-01 09:37:41 -0300

    Update personagens.py

--- personagens.py
+++ personagens.py
@@ -2,3 +2,10 @@ import PODERES
 
 class Personagens():
   def__init__(self, nome, poderes, imagem)
+  self.nome = nome
+  self.poderes = poderes
+  self.imagem = imagem
+  
+  def desenha(self, tela):
+    pass
+  
