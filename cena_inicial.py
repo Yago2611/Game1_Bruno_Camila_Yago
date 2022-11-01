b5efe32a2461bc20923c0bc0839d5b98e27e9e7e
Bruno8652 <116586486+Bruno8652@users.noreply.github.com>
2022-11-01 09:34:21 -0300

    Update cena_inicial.py

--- cena_inicial.py
+++ cena_inicial.py
@@ -8,4 +8,7 @@ class Cena_Inicial:
     self.tela = tela
     font_titulo = pg.font.SysFont(None, 12)
     self.titulo = font_titulo.render(f'Escolha um personagem', True, (0,0,0))
-  def 
+    
+    self.encerrar = False
+    
+   
