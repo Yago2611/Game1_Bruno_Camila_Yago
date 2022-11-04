49a6abcf4952a8b992f22c021775193628635945
Bruno8652 <116586486+Bruno8652@users.noreply.github.com>
2022-11-04 10:56:04 -0300

    Update jogador.py

--- jogador.py
+++ jogador.py
@@ -6,6 +6,7 @@ class Jogador:
   def __init__ (self,posicao,personagem):
     self.posicao = posicao
     self.personagem = personagem #O jogador ira receber um objeto da classe personagem
+    self.vida = personagem.vida
     self.vx = 0
     self.vy = 0
   def mover_cima(self):
@@ -18,5 +19,6 @@ class Jogador:
     self.vy = Configuracoes.VELOCIDADE
   def posicao_na_tela(self):
         self.posicao = (vx, vy)
+ 
       
 # p1 = (pass , PERSONAGENS.nikola_tesla, PERSONAGENS.raios, PERSONAGENS.nikola_tesla)
