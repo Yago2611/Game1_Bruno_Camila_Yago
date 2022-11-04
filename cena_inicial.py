214e659b0e56622602af3f9ac850f5f5ec5613ce
Yago2611 <116475207+Yago2611@users.noreply.github.com>
2022-11-04 11:24:35 -0300

    Update cena_inicial.py

--- cena_inicial.py
+++ cena_inicial.py
@@ -11,6 +11,21 @@ class Cena_Inicial:
     self.encerrar = False
     self.escolha_jog1 = False
     self.escolha_jog2 = False 
+    titulo = pg.font.SysFont(None,FONTE_TITULO)
+    escolha = pg.font.SysFont(None, FONTE_MAIOR)
+    personagens = pg.font.SysFont(None, FONTE_MENOR)
+    Titulo = titulo.render(f'Guerra de Cientistas',True,(0,0,0))
+    Escolha = escolha.render(f'Escolha um personagem:', True, (0,0,0))
+    Personagem1 = personagens.render(f'1) Nikola Tesla', True, (0,0,0))
+    Personagem2 = personagens.render(f'2) Marie Curie', True, (0,0,0))
+    tela.fill((255, 255, 255))
+    PX = LARGURA_TELA // 2 - Titulo.get_size()[0] // 2
+    PY = 0.01 * ALTURA_TELA
+    px = LARGURA_TELA // 2 - Escolha.get_size()[0] // 2
+    py = (0.2 * ALTURA_TELA // 2) + (Escolha.get_size()[1] * 1.5)
+    px_personagens = 0.05*LARGURA_TELA 
+    py1 = (ALTURA_TELA*0.3) + (Personagem1.get_size()[1] * 1.5)
+    py2 = (ALTURA_TELA*0.4) + (Personagem1.get_size()[1] * 1.5)
  
 def rodar(self):
   while not self.encerrar:
@@ -20,8 +35,8 @@ def rodar(self):
 
 def tratamento_de_eventos(self):
   for event in pg.event.get():
-        #Evento de fechar a janela
-        if event.type == (pg.QUIT) or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE) or (pg.key.get_pressed()[pg.K_ESCAPE]): 
+     #Evento de fechar a janela
+     if event.type == (pg.QUIT) or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE) or (pg.key.get_pressed()[pg.K_ESCAPE]): 
           sys.exit()
         if (event.type == pg.KEYDOWN and event.key == pg.K_DOWN):
           if posicao>=0 and posicao<Configuracoes.NUM_PERSONAGENS-1:
@@ -40,10 +55,28 @@ def tratamento_de_eventos(self):
         time.sleep(0.2)
 
 def atualiza_estado(self):
-  pass
-
+  if posicao == 0 and escolha_jog1 == False:
+        Personagem1 = personagens.render(f'1) Nikola Tesla  [Jogador 1]', True, (122,122,0))
+        tela.blit(Personagem1, (px_personagens, py1))
+    elif posicao == 0 and escolha_jog1:
+        Personagem1 = personagens.render(f'1) Nikola Tesla  [Jogador 2]', True, (122,122,0))
+        tela.blit(Personagem1, (px_personagens, py1))
+    if posicao == 1 and escolha_jog1 == False:
+        Personagem2 = personagens.render(f'2) Marie Curie [Jogador 1]', True, (122,122,0))
+        tela.blit(Personagem2, (px_personagens, py2))
+    elif posicao == 1 and escolha_jog1:
+        Personagem2 = personagens.render(f'2) Marie Curie [Jogador 2]', True, (122,122,0))
+        tela.blit(Personagem2, (px_personagens, py2))
+  if self.escolha_jog1 and self.escolha_jog2:
+    self.encerrar = True 
+    
 def desenha(self):
-  pass
+  tela.fill((255, 255, 255))
+  tela.blit(Titulo, (PX,PY))
+    tela.blit(Escolha, (px, py))
+    tela.blit(Personagem1, (px_personagens, py1))
+    tela.blit(Personagem2, (px_personagens, py2))
+  pg.display.flip() 
 
   
     
