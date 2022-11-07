901638bb5694222aa6fa5abafe0224c1389532a0
Bruno8652 <116586486+Bruno8652@users.noreply.github.com>
2022-11-07 15:27:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,25 @@ def load_image(name, colorkey=None, scale=1.0):
         image.set_colorkey(colorkey, pg.RLEACCEL)
     return image
 
+class Jogador:  
+    def __init__ (self,posicao,personagem):
+    self.posicao = posicao
+    self.personagem = personagem #O jogador ira receber um objeto da classe personagem
+    self.vida = personagem.vida
+    self.vx = 0
+    self.vy = 0
+
+  def mover_cima(self):
+    self.vx = -Configuracoes.VELOCIDADE
+  def mover_baixo(self):
+    self.vx = Configuracoes.VELOCIDADE
+  def mover_esquerda(self):
+    self.vy = -Configuracoes.VELOCIDADE
+  def mover_direita(self):
+    self.vy = Configuracoes.VELOCIDADE
+  def posicao_na_tela(self):
+        self.posicao = (vx, vy)
+
 class Personagem:
    def __init__(self, nome, imagem, poder):
     self.nome = nome
