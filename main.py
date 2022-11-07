bf14f3277ab80e40c3bca3000c1a1acaabe44797
CamilaAudi <116517580+CamilaAudi@users.noreply.github.com>
2022-11-07 14:39:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,26 @@ class Personagem:
     self.nome = nome
     self.imagem = imagem
     self.poder = poder
+    self.hp = 100
+    
+class Jogador:  
+    def __init__ (self,posicao,personagem):
+    self.posicao = posicao
+    self.personagem = personagem #O jogador ira receber um objeto da classe personagem
+    self.vida = personagem.vida
+    self.vx = 0
+    self.vy = 0
+
+  def mover_cima(self):
+    self.vx = -Configuracoes.VELOCIDADE
+  def mover_baixo(self):
+    self.vx = Configuracoes.VELOCIDADE
+  def mover_esquerda(self):
+    self.vy = -Configuracoes.VELOCIDADE
+  def mover_direita(self):
+    self.vy = Configuracoes.VELOCIDADE
+  def posicao_na_tela(self):
+        self.posicao = (vx, vy)
     
 
 def main():
@@ -201,6 +221,7 @@ def main():
     tela.blit(imagem1, (P1_X,P1_Y))
     tela.blit(imagem2, (P2_X,P2_Y))
     #Desenha os jogadores
+    
     
     #Minions
     if minion:
