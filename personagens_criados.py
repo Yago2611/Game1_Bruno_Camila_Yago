cda8d7977071323b02fc1c60198901d23b30f79f
Bruno8652 <116586486+Bruno8652@users.noreply.github.com>
2022-11-28 02:36:29 -0300

    Update personagens_criados.py

--- personagens_criados.py
+++ personagens_criados.py
@@ -2,14 +2,17 @@ from poder import Poder
 from personagem import Personagem
 
 class Personagens_criados:
-    Raio = Poder("raio.png","Projetil")
-    Nuvem = Poder("nuvem.png","Área")
-    Cura = Poder("cura.png","Cura")
-    Gato = Poder("gato.png","Projetil")
-    Nikola = Personagem("Nikola Tesla","nikola.png",Raio) 
-    Marie = Personagem("Marie Curie","marie.png",Nuvem)
-    Darwin = Personagem("Charles Darwin", "darwin.png", Cura)
-    Darwin.vida_maxima = 400 #Vantagem
-    Darwin.velocidade *= 0.5 #Desvantagem
-    Erwin = Personagem("Erwin Schrodinger","erwin.png",Gato)
-    lista = [Nikola,Marie,Darwin,Erwin]
+    def __init__(self):
+        pass
+    def cria_lista(self):
+        Raio = Poder("raio.png","Projetil")
+        Nuvem = Poder("nuvem.png","Área")
+        Cura = Poder("cura.png","Cura")
+        Gato = Poder("gato.png","Projetil")
+        Nikola = Personagem("Nikola Tesla","nikola.png",Raio) 
+        Marie = Personagem("Marie Curie","marie.png",Nuvem)
+        Darwin = Personagem("Charles Darwin", "darwin.png", Cura)
+        Darwin.vida_maxima = 400 #Vantagem
+        Darwin.velocidade *= 0.5 #Desvantagem
+        Erwin = Personagem("Erwin Schrodinger","erwin.png",Gato)
+        return [Nikola,Marie,Darwin,Erwin]
