46792e0534a59fa1fcbf29accb6fb89dfa5c9d16
Bruno8652 <116586486+Bruno8652@users.noreply.github.com>
2022-11-28 01:32:18 -0300

    Create personagens_criador.py

---
+++ personagens_criador.py
@@ -0,0 +1,15 @@
+from poder import Poder
+from personagem import Personagem
+
+class Personagens_criados:
+    Raio = Poder("raio.png","Projetil")
+    Nuvem = Poder("nuvem.png","Área")
+    Cura = Poder("cura.png","Cura")
+    Gato = Poder("gato.png","Projetil")
+    Nikola = Personagem("Nikola Tesla","nikola.png",Raio) 
+    Marie = Personagem("Marie Curie","marie.png",Nuvem)
+    Darwin = Personagem("Charles Darwin", "darwin.png", Cura)
+    Darwin.vida_maxima = 400 #Vantagem
+    Darwin.velocidade *= 0.5 #Desvantagem
+    Erwin = Personagem("Erwin Schrodinger","erwin.png",Gato)
+    lista = [Nikola,Marie,Darwin,Erwin]
