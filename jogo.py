415077752461c3468dec59e525fae7d33ad75fc4
Bruno8652 <116586486+Bruno8652@users.noreply.github.com>
2022-11-28 01:31:14 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,9 +1,15 @@
+import pygame as pg
+from cena_inicial import Cena_inicial
 from cena_principal import Cena_principal
-from configuracoes import Configuracoes
-from jogador import Jogador
+from cena_final import Cena_final
 
 class Jogo:
   def __init__(self):
         pg.init()
-        self.tela = pg.display.set_mode(     
-            (Configuracao.LARGURA_TELA, Configuracao.ALTURA_TELA))
+  def rodar(self):
+   cena_inicial = Cena_inicial()
+   cena_inicial.rodar()
+   cena_principal = Cena_principal(cena_inicial)
+   cena_principal.rodar()
+   cena_final = Cena_final(cena_principal)
+   cena_final.rodar()
