9f4850c53b60dc399e0a93af29f285cca507f2a7
CamilaAudi <116517580+CamilaAudi@users.noreply.github.com>
2022-12-11 15:21:57 -0300

    Update personagens_criados.py

--- personagens_criados.py
+++ personagens_criados.py
@@ -8,11 +8,17 @@ class Personagens_criados:
         Raio = Poder("raio.png","Projetil")
         Nuvem = Poder("nuvem.png","Área")
         Cura = Poder("cura.png","Cura")
-        Gato = Poder("gato.png","Projetil")
+        Teletransporte = Poder("tunel.png","Teletransporte")
         Nikola = Personagem("Nikola Tesla","nikola.png",Raio) 
+        Nikola.dano_dado = 20 #Vantagem
+        Nikola.reconhecimento = 3 #Desvantagem (Apenas um a cada três ataques são reconhecidos pela comunidade científica)
         Marie = Personagem("Marie Curie","marie.png",Nuvem)
+        Marie.dano_dado = 5 #Vantagem
+        Marie.dano_recebido = 1/2 #Desvantagem
         Darwin = Personagem("Charles Darwin", "darwin.png", Cura)
         Darwin.vida_maxima = 400 #Vantagem
         Darwin.velocidade *= 0.5 #Desvantagem
-        Erwin = Personagem("Erwin Schrodinger","erwin.png",Gato)
+        Erwin = Personagem("Erwin Schrodinger","erwin.png",Teletransporte)
+        Erwin.probabilidade = 2 #Vantagem (Só recebe um dano a cada dois)
+        Erwin.tempo_parado = 10 #Desvantagem (Para a cada 10 segundos para tossir e perde vida)
         return [Nikola,Marie,Darwin,Erwin]
